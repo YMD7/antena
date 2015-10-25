476d7c7205bb5701a54468af17ca4452f5adb920
Kyo Yamada <hysteric.madam@gmail.com>
2015-10-25 23:28:34 +0900

    管理画面の作成を開始

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   root to: 'pages#index'
+
   match 'gate', :to => 'pages#login_gate', :via => 'get'
-  devise_for :users,
-    controllers: { omniauth_callbacks: "omniauth_callbacks" }
+  match 'admin', :to => 'admin#home', :via => 'get'
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks",
+    invitations: "users/invitations"
+  }
+
 end
