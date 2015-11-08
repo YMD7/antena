7258bc57c3cabd0cff93566c9d0c3b1d267d2183
Kyo Yamada <hysteric.madam@gmail.com>
2015-11-09 02:00:03 +0900

    currentクラスを追加するヘルパーを作成中

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,7 @@
 module AdminHelper
+  def controller_name_currenter(el, name)
+    if name === controller_name
+      '<li>'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,25 @@
 Rails.application.routes.draw do
 
+  # -- root --
   root to: 'static_pages#top'
-  get 'gate', :to => 'static_pages#gate'
 
-  devise_for :users, controllers: {
-    omniauth_callbacks: "omniauth_callbacks",
-    invitations: "users/invitations"
-  }
+  # -- static pages --
+  get 'gate',  :to => 'static_pages#gate'
 
+  # -- admin --
+  get 'admin', :to => 'admin/posts#index'
   namespace :admin do
     resources :posts
     resources :users
   end
 
+  # -- resources --
   resources :posts
+
+  # -- devise --
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks",
+    invitations: "users/invitations"
+  }
 
 end
