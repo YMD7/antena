d34acc091e17a87e1cdf910899613ccb8dc7c3e2
Kyo Yamada <hysteric.madam@gmail.com>
2015-11-26 00:02:02 +0900

    左上にユーザーアイコンを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
   def show
-    @need_menu_and_project = true
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
   def top
-    @need_menu_and_project = true
   end
 
   def gate
-    @need_menu_and_project = true
   end
 end
