2404d64aed49479e759b2211057c87d30c8a4db3
Kyo Yamada <hysteric.madam@gmail.com>
2015-12-03 00:38:21 +0900

    i18n対応の準備完了

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    @user   = User.new
+    @mailer = YAML.load(File.open("config/locales/devise_invitable.en.yml"))["en"]["devise"]["mailer"]
   end
 end
