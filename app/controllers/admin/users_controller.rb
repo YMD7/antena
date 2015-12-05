f953a8ee02da23ba0708ff41c41ed1ddf511554a
Kyo Yamada <hysteric.madam@gmail.com>
2015-12-05 22:13:05 +0900

    招待メールのプレビュー文を表示

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,7 @@ class Admin::UsersController < ApplicationController
 
   def new
     @user   = User.new
-    @mailer = YAML.load(File.open("config/locales/devise_invitable.en.yml"))["en"]["devise"]["mailer"]
+    @mailer = YAML.load(File.open("config/locales/devise_invitable.ja.yml"))["ja"]["devise"]["mailer"]["invitation_instructions"]
+    @mailer["someone_invited_you"].gsub!(/%\{url\}/, "\##{root_url}\#")
   end
 end
