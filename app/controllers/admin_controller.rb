2e92840e0f61971f09de67d163b18656648af2e5
Kyo Yamada <hysteric.madam@gmail.com>
2015-12-30 17:13:35 +0900

    admin左上のユーザー表示部分を実装

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,21 @@
 class AdminController < ApplicationController
   layout 'admin'
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :user
+
+  def user
+    @user = current_user
+
+    @role          = '権限未設定'
+    @role          = '管理者' if @user.role == 'admin'
+    @role          = '投稿者' if @user.role == 'author'
+    @username      = @user.username
+    @fullname_jp   = "#{@user.family_name_jp} #{@user.first_name_jp}" if @user.family_name_jp && @user.first_name_jp
+    @affiliate     = @user.affiliate
+    @icon_filename = @username.gsub(' ', '_')
+    file_dir  = "app/assets/images/user_icons/"
+    file_path = file_dir + @icon_filename + ".jpg"
+    unless FileTest.exist?(file_path) then @icon_filename = 'no_icon' end
+  end
 
   def home
   end
