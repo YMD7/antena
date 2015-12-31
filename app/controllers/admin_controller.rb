2a5718b00eec0870bb5c5b98e015727b070b73c7
Kyo Yamada <hysteric.madam@gmail.com>
2015-12-31 20:58:21 +0900

    ログイン中のユーザーを最初に並べる、アイコン未登録者に対応

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,16 +5,13 @@ class AdminController < ApplicationController
   def user
     @admin_user = current_user
 
+    @id            = @admin_user.id
     @role          = '権限未設定'
     @role          = '管理者' if @admin_user.role == 'admin'
     @role          = '投稿者' if @admin_user.role == 'author'
     @username      = @admin_user.username
-    @fullname_jp   = "#{@admin_user.family_name_jp} #{@admin_user.first_name_jp}" if @admin_user.family_name_jp && @admin_user.first_name_jp
+    @fullname_jp   = "#{@admin_user.family_name_jp} #{@admin_user.first_name_jp}" unless @admin_user.family_name_jp + @admin_user.first_name_jp == ''
     @affiliate     = @admin_user.affiliate
-    @icon_filename = @username.gsub(' ', '_')
-    file_dir  = "app/assets/images/user_icons/"
-    file_path = file_dir + @icon_filename + ".jpg"
-    unless FileTest.exist?(file_path) then @icon_filename = 'no_icon' end
   end
 
   def home
